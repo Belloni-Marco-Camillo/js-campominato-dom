86dcbc8b01aa3a46cec99003e55b3ca6d69c3427
Belloni-Marco-Camillo <bellonimarcocamillo@gmail.com>
2022-04-07 10:10:25 +0200

    end_commit

--- asset/js/main.js
+++ asset/js/main.js
@@ -34,27 +34,34 @@ function show() {
         const grid = document.getElementById(`grid`) //creo una costante dove inserisco la mia ul
         grid.innerHTML = ' '; //inizializzo la grigia vuota
         let x = 100; //gli do il numero delle celle che deve andarmi a generare
+
         for (let i = 1; i <= x; i++) {
             const listItem = document.createElement(`li`); //creo l'elemento da inserire
             const point = document.getElementById(`grid`).appendChild(listItem); //inserisco in una variabile l'elemento appena crato
+
             point.append(number) //gli aggiungo i numeri che ho inizializzato a 1 all'inizio del ciclo if
             randomNumeber(1, 100);
+
             if (RandomArray.includes(i)) {
                 console.log(`il numero ${i} è una bomba`);
                 point.addEventListener('click', function colour() { //aggiungo una funzione che mi permetta di cambiare colore all'elemento quando lo clicco aggiungendogli una classe
                     point.classList.add(`bomb`) //gli aggiungo la classe (creata in css)
                     alert `game over`
-                    alert `hai indovintato ` + progress
+                    alert("hai indovinato " + progress)
                     location.reload();
                 });
             }
+
             point.classList.add(`listStyle`)
             point.addEventListener('click', function colour() { //aggiungo una funzione che mi permetta di cambiare colore all'elemento quando lo clicco aggiungendogli una classe
                 point.classList.add(`green`)
                 progress++ //gli aggiungo la classe (creata in css)
             });
+
             number++ //incremento il mio contatore
         }
+
+
     } else if (level == `medium`) { //creo un ciclo if che mi chieda che valore è stato selezionata
         let number = 1 //inizializzo i numeri da inserire
         grid.innerHTML = ' '; //inizializzo la grigia vuota //inizializzo la grigia vuota
